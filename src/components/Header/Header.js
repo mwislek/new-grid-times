a6143e04e0c17a9d6540363c21e22229a087eab7
Mike <mike.wislek@gmail.com>
2023-04-25 17:05:51 -0500

    feat: correct logo size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
     <header>
       <SuperHeader>
         <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
+          <Actions />
           <ActionGroup>
             <button>
               <User size={24} />
@@ -31,14 +24,40 @@ const Header = () => {
       <MainHeader>
         <Logo />
       </MainHeader>
+
+      <DesktopHeader>
+        <Actions />
+        <Filler />
+        <Logo />
+        <Filler />
+        <SubscribeActions>
+          <Button>Subscribe</Button>
+          <LoginButtonLink>Already a subscriber?</LoginButtonLink>
+        </SubscribeActions>
+      </DesktopHeader>
     </header>
   );
 };
+
+const Actions = () => (
+  <ActionGroup>
+    <button>
+      <Search size={24} />
+    </button>
+    <button>
+      <Menu size={24} />
+    </button>
+  </ActionGroup>
+);
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +84,38 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
+
+const DesktopHeader = styled(MaxWidthWrapper)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    align-items: baseline;
+    margin-bottom: 72px;
+    margin-top: 16px;
+  }
+`;
+
+const SubscribeActions = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+const LoginButtonLink = styled.button`
+  color: ${COLORS.gray[900]};
+  font-style: italic;
+  text-decoration-line: underline;
+`;
+
+const Filler = styled.div`
+  flex: 1;
+`
 
 export default Header;
